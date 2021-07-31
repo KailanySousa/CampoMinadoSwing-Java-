0ebf3dc72852777e31de47ceb81c8ae706513592
KailanySousa <kailany-sousa2010@hotmail.com>
2021-07-30 21:40:31 -0300

    implementando interface gráfica #01

---
+++ src/br/com/ksg/cm/view/Main.java
@@ -0,0 +1,29 @@
+package br.com.ksg.cm.view;
+
+import javax.swing.JFrame;
+
+import br.com.ksg.cm.model.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class Main extends JFrame{
+	
+	public Main() {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(16, 30, 50);
+		PainelTabuleiro pnl = new PainelTabuleiro(tabuleiro);
+		
+		add(pnl);
+		setTitle("Campo Minado");
+		setSize(690, 440);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		
+		new Main();
+
+	}
+
+}
---
+++ src/br/com/ksg/cm/view/PainelTabuleiro.java
@@ -0,0 +1,13 @@
+package br.com.ksg.cm.view;
+
+import javax.swing.JPanel;
+
+import br.com.ksg.cm.model.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+
+	public PainelTabuleiro(Tabuleiro tabuleiro) {
+		
+	}
+}
